225532e364255028b63b5f062de1374ce8c28d6f
Kevin <1075436260@qq.com>
2017-12-24 19:08:23 +0800

    resumeN0.1

---
+++ js/index.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  var toTop       = document.getElementById('to-top'),
+      timer       = null;
+  window.onscroll = function() {
+    var toNum = document.documentElement.scrollTop || document.body.scrollTop;
+    if (toNum > 100) {
+      toTop.style.display = "block";
+    }else {
+      toTop.style.display = "none";
+    }
+  }
+  toTop.onclick = function() {
+    timer = setInterval(function() {
+      var toNum = document.documentElement.scrollTop || document.body.scrollTop,
+          speed = Math.ceil(toNum/5);
+      document.documentElement.scrollTop = document.body.scrollTop = toNum - speed;
+      if (toNum == 0) {
+        clearInterval(timer);
+      }
+    },50)
+  }
+  // nav hover
+  var navText = document.getElementsByClassName("nav-text");
+  for (var i = 0; i < navText.length; i++) {
+    navText[i].onmouseover = function() {
+      this.classList.add("active");
+    }
+    navText[i].onmouseout = function() {
+      this.classList.remove("active");
+    }
+  }
+
+}
